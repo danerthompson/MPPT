def0fb530ed0d11a975fa9b9a9125977379a1365
jimbob9245 <jimhgant@gmail.com>
2022-11-09 17:38:19 -0600

    change
    change

--- MPPT ESP 32 Code/src/main.cpp
+++ MPPT ESP 32 Code/src/main.cpp
@@ -2,27 +2,44 @@
 
 
 int dutyCycle; //0 to 255
-int inputpin = 1; //GPIO1
-int outputpin = 0; //GPIO0
-int channel = 0; //0 to 15
+//int inputpin = 1; //GPIO1
+int outputpin = 1; //GPIO
+int channel = 2; //0 to 15
 int freq = 50000; //
 int resolution = 8; //
+unsigned long timer;
 
 void setup(){
-pinMode(inputpin, INPUT); 
+//pinMode(inputpin, INPUT); 
+  gpio_reset_pin(GPIO_NUM_1);
+  pinMode(outputpin, OUTPUT);
   // configure LED PWM functionalitites
   ledcSetup(outputpin, freq, resolution);
   
   // attach the channel to the GPIO to be controlled
   ledcAttachPin(outputpin, channel);
 
+  timer = micros();
+
   Serial.begin(9600);
 
 }
 
 void loop(){
-dutyCycle = analogRead(inputpin)*255/4096;
-ledcWrite(outputpin, dutyCycle); //
-Serial.println(dutyCycle);
+//dutyCycle = analogRead(inputpin)*255/4096;
+
+
+
+if((micros()-timer)>=10000)
+{
+  dutyCycle++;
+  if(dutyCycle >= 255)
+  {
+    dutyCycle = 0;
+  }
+  ledcWrite(outputpin, dutyCycle); //
+  Serial.println(dutyCycle);
+  timer+=10000;
+}
 
 }
