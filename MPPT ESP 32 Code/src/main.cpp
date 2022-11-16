232d859f68c396f51df45d7f8333035e8e6129d8
danerthompson <58756948+danerthompson@users.noreply.github.com>
2022-11-16 16:52:27 -0600

    Added some stuff

--- MPPT ESP 32 Code/src/main.cpp
+++ MPPT ESP 32 Code/src/main.cpp
@@ -5,8 +5,11 @@ int dutyCycle; //0 to 255
 int inputpin = 1; //GPIO1
 int outputpin = 0; //GPIO0
 int channel = 0; //0 to 15
-int freq = 100000; //I dont know the range, can at least go up to 5khz
+int freq = 75000; //I dont know the range, can at least go up to 5khz
 int resolution = 8; //
+int currentPin = 2; // Current sensor ADC input
+float adcErr = 0;
+float currentADC;
 
 void setup(){
 pinMode(inputpin, INPUT); 
@@ -18,11 +21,23 @@ pinMode(inputpin, INPUT);
 
   Serial.begin(9600);
 
+  adcErr = 2.5 - float(analogRead(currentPin)*3.3/4096);
+
 }
 
 void loop(){
 dutyCycle = analogRead(inputpin)*255/4096;
-ledcWrite(outputpin, dutyCycle); //
+ledcWrite(outputpin, 100); //
+
+Serial.print("Duty cycle: ");
 Serial.println(dutyCycle);
+
+currentADC = float(analogRead(currentPin)*3.3/4096)+adcErr;
+  Serial.print("Current sensor");
+  Serial.println(float(analogRead(currentPin)*3.3/4096)+adcErr);
+
+
+  Serial.print("Current (AMPS): ");
+  Serial.println((2.5-currentADC)/0.4);
 
 }
